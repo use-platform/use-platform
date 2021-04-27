3df4ffd28a7838bd78302f790c100dfd200e596a
yarastqt <yarastqt@gmail.com>
2021-04-27 20:26:10 +0300

    chore: add webpack alias for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,9 @@
+const path = require('path')
+
 module.exports = {
   stories: ['../src/**/*.examples.@(ts|tsx)'],
+  webpackFinal: (config) => {
+    config.resolve.alias['@yandex/web-platform'] = path.resolve(__dirname, '../src')
+    return config
+  },
 }
