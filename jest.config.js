cb7b8f76afc08e47df06450b4c8cc13c76145958
yarastqt <yarastqt@gmail.com>
2021-05-17 18:33:38 +0300

    chore: use es6 tsconfig for jest

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@ module.exports = {
   preset: 'ts-jest',
   testMatch: ['<rootDir>/**/*.test.{ts,tsx}'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.test.json',
+    },
+  },
 }
