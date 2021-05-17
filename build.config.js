944ed780149f978eb479546417886f716c50505a
yarastqt <yarastqt@gmail.com>
2021-05-17 18:15:37 +0300

    chore: add bem-react/pack for build

---
+++ build.config.js
@@ -0,0 +1,30 @@
+const { useCleanUpPlugin } = require('@bem-react/pack/lib/plugins/CleanUpPlugin')
+const { useCopyAssetsPlugin } = require('@bem-react/pack/lib/plugins/CopyAssetsPlugin')
+const { useTypeScriptPlugin } = require('@bem-react/pack/lib/plugins/TypescriptPlugin')
+
+/**
+ * @type {import('@bem-react/pack/lib/interfaces').Config}
+ */
+module.exports = {
+  output: './dist',
+
+  plugins: [
+    useCleanUpPlugin(['./dist']),
+
+    useTypeScriptPlugin({
+      configPath: './tsconfig.json',
+      onCreateSideEffects: () => false,
+    }),
+
+    useCopyAssetsPlugin([
+      {
+        context: './src',
+        src: './**/*.{md}',
+        output: ['./dist', './dist/esm'],
+      },
+      {
+        src: ['./package.json', './readme.md'],
+      },
+    ]),
+  ],
+}
