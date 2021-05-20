abeb9829e150e20d0802ed10e65d5cab7afb5255
yarastqt <yarastqt@gmail.com>
2021-05-20 16:48:13 +0300

    chore: update storybook and add addon-essentials deps

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,7 @@ const path = require('path')
 
 module.exports = {
   stories: ['../src/**/*.examples.@(ts|tsx)'],
+  addons: ['@storybook/addon-essentials'],
   webpackFinal: (config) => {
     config.resolve.alias['@yandex/web-platform'] = path.resolve(__dirname, '../src')
     return config
