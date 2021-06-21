7aff2b8756032d58fe85f781e683e36c194f4bb2
yarastqt <yarastqt@gmail.com>
2021-06-21 17:55:59 +0300

    fix(press): use prevent for all except check or radio input

--- src/shared/usePress/utils/keyboard-event.ts
+++ src/shared/usePress/utils/keyboard-event.ts
@@ -1,9 +1,15 @@
+export function isCheckableInput(element: HTMLElement): boolean {
+  const type = element.getAttribute('type')
+
+  return element.tagName === 'INPUT' && (type === 'checkbox' || type === 'radio')
+}
+
 export function isValidKeyboardEvent(event: KeyboardEvent): boolean {
   const { key, target } = event
   const element = target as HTMLElement
 
   const role = element.getAttribute('role')
-  const isHTMLAnchorLink = element.tagName === 'A' && element.hasAttribute('href')
+  const isLink = element.tagName === 'A' && element.hasAttribute('href')
 
   // Accessibility for keyboards. Space and Enter only.
   // "Spacebar" is for IE 11
@@ -11,7 +17,7 @@ export function isValidKeyboardEvent(event: KeyboardEvent): boolean {
     (key === 'Enter' || key === ' ' || key === 'Spacebar') &&
     // A link with a valid href should be handled natively,
     // unless it also has role='button' and was triggered using Space.
-    (!isHTMLAnchorLink || (role === 'button' && key !== 'Enter')) &&
+    (!isLink || (role === 'button' && key !== 'Enter')) &&
     // An element with role='link' should only trigger with Enter key
     !(role === 'link' && key !== 'Enter')
   )
