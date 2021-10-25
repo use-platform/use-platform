4e819da1029f77326679da499a84e415802995c1
yarastqt <yarastqt@gmail.com>
2021-10-25 14:21:20 +0300

    fix(press): skip press for contentediable element

--- src/interactions/press/utils/keyboard-event.ts
+++ src/interactions/press/utils/keyboard-event.ts
@@ -10,11 +10,14 @@ export function isValidKeyboardEvent(event: KeyboardEvent): boolean {
 
   const role = element.getAttribute('role')
   const isLink = element.tagName === 'A' && element.hasAttribute('href')
+  // Use getAttribute instead isContentEditable because jsdom doesn't support this attr.
+  const contentEditable = element.getAttribute('contenteditable')
 
   // Accessibility for keyboards. Space and Enter only ("Spacebar" is for IE 11).
   return (
     // An checkable input should only trigger with Space key.
     ((!isCheckableInput(element) && key === 'Enter') || key === ' ' || key === 'Spacebar') &&
+    contentEditable !== 'true' &&
     // A link with a valid href should be handled natively,
     // unless it also has role='button' and was triggered using Space.
     (!isLink || (role === 'button' && key !== 'Enter')) &&
