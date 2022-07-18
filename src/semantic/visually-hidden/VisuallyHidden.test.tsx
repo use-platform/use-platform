ae4410da654d0a729fab94d8351ddc7a9993f366
Maxim Stepanov <superstepashka1981@gmail.com>
2022-07-18 13:11:19 +0300

    test: remove unit test, change description

--- src/semantic/visually-hidden/VisuallyHidden.test.tsx
+++ src/semantic/visually-hidden/VisuallyHidden.test.tsx
@@ -3,24 +3,8 @@ import { VisuallyHidden } from './VisuallyHidden'
 
 const hiddenText = 'Hidden text'
 
-describe('useFocusTrap', () => {
+describe('VisuallyHidden', () => {
   const render = createClientRender()
-
-  test('should render element with proper styles', () => {
-    render(<VisuallyHidden>{hiddenText}</VisuallyHidden>)
-    expect(screen.getByText('Hidden text')).toHaveStyle({
-      border: 0,
-      clip: 'rect(0 0 0 0)',
-      clipPath: 'inset(50%)',
-      height: '1px',
-      margin: '0 -1px -1px 0',
-      overflow: 'hidden',
-      padding: 0,
-      position: 'absolute',
-      width: '1px',
-      whiteSpace: 'nowrap',
-    })
-  })
 
   test('should match snapshot', () => {
     render(
