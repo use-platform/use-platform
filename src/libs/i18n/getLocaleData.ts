bc40ec6a09a8d36b490c7325bbb84efcda2f8669
Kirill <kivahnin@gmail.com>
2022-10-11 19:10:54 +0300

    fix: removed ts-ignore

--- src/libs/i18n/getLocaleData.ts
+++ src/libs/i18n/getLocaleData.ts
@@ -7,9 +7,7 @@ const RTL_SCRIPTS = new Set(['Arab', 'Syrc', 'Samr', 'Mand', 'Thaa', 'Mend', 'Nk
 const RTL_LANGS = new Set(['ae', 'ar', 'arc', 'bcc', 'bqi', 'ckb', 'dv', 'fa', 'glk', 'he', 'ku', 'mzn', 'nqo', 'pnb', 'ps', 'sd', 'ug', 'ur', 'yi'])
 
 export function getLocaleData(locale: string): LocaleData {
-  // @ts-ignore
   if (Intl.Locale) {
-    // @ts-ignore
     const { region, script, language } = new Intl.Locale(locale).maximize()
     const direction = RTL_SCRIPTS.has(script || '') ? 'rtl' : 'ltr'
 
